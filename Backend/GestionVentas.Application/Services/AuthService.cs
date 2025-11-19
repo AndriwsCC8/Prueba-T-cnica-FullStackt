ddc88a49fa8cfebb8281095540f978a8566f863c
AndriwsCC8 <andriwscastillo98@gmail.com>
2025-11-19 14:14:20 -0400

    feat: Implementación completa del flujo de autenticación JWT (Login/Register) con Identity

--- Backend/GestionVentas.Application/Services/AuthService.cs
+++ Backend/GestionVentas.Application/Services/AuthService.cs
@@ -15,6 +15,7 @@ namespace GestionVentas.Application.Services.Auth
 
         public AuthService(UserManager<IdentityUser> userManager, IConfiguration config)
         {
+            // Inyección de dependencias: UserManager para Identity y IConfiguration para JWT
             _userManager = userManager;
             _config = config;
         }
@@ -23,6 +24,7 @@ namespace GestionVentas.Application.Services.Auth
         {
             var user = new IdentityUser
             {
+                // codigo de creación de usuario 
                 UserName = request.Email,
                 Email = request.Email
             };
@@ -50,9 +52,10 @@ namespace GestionVentas.Application.Services.Auth
 
         private string GenerateToken(IdentityUser user)
         {
+            // Generación de la clave de seguridad JWT usando la configuración
             var key = new SymmetricSecurityKey(
                 Encoding.UTF8.GetBytes(_config["Jwt:Key"]!)
-            );
+            ); 
 
             var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
 
@@ -61,7 +64,7 @@ namespace GestionVentas.Application.Services.Auth
                 new Claim(JwtRegisteredClaimNames.Sub, user.Email!),
                 new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
             };
-
+                // Configura el Token con Issuer, Audience, Claims y la expiración de 2 horas
             var token = new JwtSecurityToken(
                 issuer: _config["Jwt:Issuer"],
                 audience: _config["Jwt:Audience"],
